f619564b954ee9925083b8dd92ea6ff5e49237fd
finn <canadienfinn@gmail.com>
2023-07-06 21:03:33 +0200

    fix typo

--- src/check_expr.cpp
+++ src/check_expr.cpp
@@ -4114,7 +4114,7 @@ gb_internal void convert_to_typed(CheckerContext *c, Operand *operand, Type *tar
 				operand->mode = Addressing_Invalid;
 				convert_untyped_error(c, operand, target_type);
 				if (count > 0) {
-					error_line("'%s' is a union which only excepts the following types:\n", type_str);
+					error_line("'%s' is a union which only expects the following types:\n", type_str);
 					error_line("\t");
 					for (i32 i = 0; i < count; i++) {
 						Type *v = t->Union.variants[i];
